0bd93dccc2d1955b3cf2d29cdfb17eed9814f19c
Dannar Mawardi <dmawardi@gmail.com>
2019-07-14 18:17:04 +1000

    CSS/Bootstrap connections loaded, pseudocode JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var wins = 0;
+var losses = 0;
+countdownRunning = false;
+
+
+// Display start screen with start button
+
+// User presses start button to begin game
+
+// Card shown with questions and answers. 
+// Unpack data, display answers and create data attributes. Start countdown timer. 
+
+// if user clicks an answer, check if it's correct
+// Check data value of clicked element and compare to correct answer
+// If user selects correct answer, increment wins, show congrats screen for 5 seconds, show next question
+// If countdown finishes, lose.
+
