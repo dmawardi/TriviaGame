f25405a72561ae10c6b0d9fc614174417210494f
Dannar Mawardi <dmawardi@gmail.com>
2019-07-17 15:20:35 +1000

    Loaded questions functional

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,182 +1,182 @@
 var questions = [{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+        question: ' In the "Thundergun Express" episode, the gang is determined to make it to the movie theater in time to see the show. Who is the only member of the gang who never makes it?',
+        answers: {
+            a: 'Frank',
+            b: 'Dee',
+            c: 'Mac',
+            d: 'Charlie'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 2',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'kullie'
+    {
+        question: "When the waitress is taking Charlie's coffee order in episode 1, what name does she use to identify Charlie?",
+        answers: {
+            a: 'Chuck',
+            b: 'Larry',
+            c: 'Hitler',
+            d: 'Beethoven'
+        },
+        correctAnswer: 'c',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 3',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bully'
+    {
+        question: "In the episode 'Underage Drinking: A National Concern', what was the plan to protect the new underage patrons of Paddy's pub?",
+        answers: {
+            a: '3 drink maximum',
+            b: 'Identification',
+            c: 'Smuggling',
+            d: 'Serve them water'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: 'According to Dennis, what does the D in "The "D.E.N.N.I.S. System" stand for?',
+        answers: {
+            a: 'Disease-free',
+            b: 'Demonstrate your value ',
+            c: 'Desecrate',
+            d: 'Destruction'
+        },
+        correctAnswer: 'b',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "At the beginning of the episode Mac and Dennis go to Dee's apartment together. What is it that they went there to get?",
+        answers: {
+            a: 'Candy',
+            b: 'Popcorn',
+            c: 'A big bowl',
+            d: 'Duster'
+        },
+        correctAnswer: 'c',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "Which 'Sunny' character does Dennis hit with his car, halting the gang's trip to the strip club?",
+        answers: {
+            a: 'Frank',
+            b: 'Mac',
+            c: 'Dee',
+            d: 'Charlie'
+        },
+        correctAnswer: 'd',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: 'Who does Charlie first tell he has cancer?',
+        answers: {
+            a: 'The Waitress',
+            b: 'Cricket',
+            c: 'Charlie',
+            d: 'Dennis'
+        },
+        correctAnswer: 'd',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "Which one of the characters is not in season one?",
+        answers: {
+            a: 'Frank',
+            b: 'Dennis',
+            c: 'Mac',
+            d: 'Charlie'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "Which one of the gang quotes the Bible?",
+        answers: {
+            a: 'Mac',
+            b: 'Charlie',
+            c: 'Frank',
+            d: 'Dee'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "Paddy's Pub hosts a modeling contest for a promotional billboard. Which member of the gang is determined to win the contest himself?",
+        answers: {
+            a: 'Charlie',
+            b: 'Mac',
+            c: 'Dennis',
+            d: 'Frank'
+        },
+        correctAnswer: 'c',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "In the episode 'Dennis and Dee Go on Welfare', what drug did Dennis and Dee get hooked on to prove they needed welfare?",
+        answers: {
+            a: 'Mushrooms',
+            b: 'Crack',
+            c: 'Weed',
+            d: 'Heroin'
+        },
+        correctAnswer: 'b',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "Mac and Charlie think it's a great idea to write their own movie. While in the Paddy's Pub office, they begin brainstorming ideas for the film. Who do they decide will play their lead actor?",
+        answers: {
+            a: 'Dolph Lundgren',
+            b: 'Brad Pitt',
+            c: 'Mel Gibson',
+            d: 'Bruce Willis'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "How does Charlie try to demonstrate his value to the waitress the first time in the D.E.N.N.I.S episode?",
+        answers: {
+            a: 'By fixing her sink',
+            b: 'By buying her flowers',
+            c: 'By biting at her heels',
+            d: 'By berating her'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "What movie did Dennis rent that upsets Mac because he wasn't consulted about the decision?",
+        answers: {
+            a: 'Die Hard',
+            b: 'Pulp Fiction',
+            c: 'Transporter 2',
+            d: 'The Italian Job'
+        },
+        correctAnswer: 'c',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "What type of community service are Dennis, Mac, and Dee assigned to after throwing the bag of burning poo at the building next door?",
+        answers: {
+            a: 'Coaching youth basketball',
+            b: 'Cleaning up the beach',
+            c: 'Garbage Collecting',
+            d: 'Directing traffic'
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "What is the alias that Dennis comes up with for Charlie, when he and Mac are entering him into the underground fight tournament?",
+        answers: {
+            a: 'Iron Skull',
+            b: 'Clown Baby',
+            c: 'Reaper Dummy',
+            d: 'Lucky Sam'
+        },
+        correctAnswer: 'b',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "In season 7, the gang goes to the Jersey Shore, and Charlie gets to spend a whole night with the waitress. How does this happen?",
+        answers: {
+            a: 'The waitress is on ecstasy',
+            b: "They don't, it's all in Charlie's head",
+            c: 'He spikes her drink',
+            d: "She's attacked by rabid dogs"
+        },
+        correctAnswer: 'a',
     },
-    correctAnswer: 'c',
-},
-{
-    question: 'Question 1',
-    answers: {
-        a: 'honolulu',
-        b: 'slavery',
-        c: 'correct',
-        d: 'bahlul'
+    {
+        question: "What does Frank use to cut his toenails that disgusts Mac?",
+        answers: {
+            a: 'Scissors',
+            b: 'Old nail clippers',
+            c: 'Swiss Army Knife',
+            d: 'Steak knife'
+        },
+        correctAnswer: 'd',
     },
-    correctAnswer: 'c',
-},
 
 ]
